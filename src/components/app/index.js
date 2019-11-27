1a6ae2ed65ac66996c0406c5be4ec75b1f3f8dea
dpislari <dpislari@endava.com>
2019-11-27 13:55:14 +0200

    Page initial layout added

---
+++ src/components/app/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Layout from "../layout";
+
+import "antd/dist/antd.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
