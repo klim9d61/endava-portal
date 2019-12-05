5540cdad6fa87c53327d7c754d8138bb45baf4ef
CapceleaCristian <chridan@mail.ru>
2019-12-05 22:57:56 +0200

    Add calendar component, page and temp event
    Minor style for calendar
    
    Add calendarConfig file with calendar configuration datas

---
+++ src/features/calendar-events/calendarConfig.js
@@ -0,0 +1,24 @@
+import { momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+
+const localizer = momentLocalizer(moment)
+const defaultDate = new Date()
+
+const config = {
+  defaultDate,
+  drilldownView: 'agenda',
+  defaultView: 'month',
+  events: [
+    {
+      start: new Date(2019, 11, 12),
+      end: new Date(2019, 11, 12),
+      title: 'XMAS Party',
+      allDay: true,
+    },
+  ],
+  localizer,
+  resizable: true,
+  style: { padding: 15, width: '90%', margin: '0 auto', height: '85vh' },
+}
+
+export default config
