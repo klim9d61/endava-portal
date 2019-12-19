f475312e58f16ed0761e87efd0b5742f8d59ffc0
mirabela-t <mirabela.tarai@gmail.com>
2019-12-19 14:47:39 +0200

    add 404 page

--- src/pages/notFound/index.js
+++ src/pages/notFound/index.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import { Result, Icon } from 'antd'
 
-function NotFoundPage() {
-  return <div>404 Not Found</div>
+import { FormButton } from '../login/styles'
+
+const NotFoundPage = ({ history }) => {
+  return (
+    <div style={{ padding: '40px 0', height: '100vh' }}>
+      <Result
+        style={{ paddingTop: '80px', zoom: '1.1' }}
+        icon={<Icon style={{ fontSize: '90px' }} type="frown" theme="filled" />}
+        title="404"
+        subTitle="The page you visited does not exist."
+        extra={
+          <FormButton
+            style={{ width: '150px' }}
+            onClick={() => history.push('/')}
+            type="primary"
+          >
+            Back Home
+          </FormButton>
+        }
+      />
+    </div>
+  )
 }
 
 export default NotFoundPage
